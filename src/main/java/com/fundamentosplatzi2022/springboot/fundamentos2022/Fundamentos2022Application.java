e079d878002c5aec45c6ee6ad2656c375024e935
DESKTOP-LK5G5KM\Sanchez <Miguel Sanchez>
2022-06-06 18:53:50 -0500

    first commit

---
+++ src/main/java/com/fundamentosplatzi2022/springboot/fundamentos2022/Fundamentos2022Application.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi2022.springboot.fundamentos2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundamentos2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundamentos2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi2022/springboot/fundamentos2022/Fundamentos2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi2022.springboot.fundamentos2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fundamentos2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
